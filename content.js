89304e37c1b0a87bc6eb30745ccbd27797962c10
test <test123>
2019-11-24 22:09:57 +0100

    new version of dictionary page - fix

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
-$(".pron-info").each( function() {
+$(".dpron-i").each( function() {
 	
 	var downloadIcon = chrome.extension.getURL("download.png");
 	console.log(downloadIcon);
-	var urlToFile = $(this).find(".sound").attr("data-src-mp3");
+	var urlToFile = $(this).find("source[type='audio/mpeg']").attr("src");
+	console.log(urlToFile);
 	var downloadButtonHtml = "<a href=\""+urlToFile+"\" download><img alt=\"download\" src=\""+downloadIcon+"\"></a>";
 	$(this).find( "span" ).first().append( downloadButtonHtml);
 	
