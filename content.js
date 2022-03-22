76ced4c8f83c3416f13ddd85917a4318838b238c
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2022-03-22 11:43:58 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,21 @@
 $(".dpron-i").each( function() {
 	
 	var downloadIcon = chrome.extension.getURL("download.png");
-	console.log(downloadIcon);
 	var urlToFile = $(this).find("source[type='audio/mpeg']").attr("src");
 	console.log(urlToFile);
 	var downloadButtonHtml = "<a href=\""+urlToFile+"\" download><img alt=\"download\" src=\""+downloadIcon+"\"></a>";
 	$(this).find( "span" ).first().append( downloadButtonHtml);
 	
+});
+
+$(".hws h1").each( function() {
+
+	var downloadIcon = chrome.extension.getURL("download.png");
+	var urlToFile = $(this).find(".audioIcon").attr("data-audio-url");
+	console.log(urlToFile);
+	var downloadButtonHtml = "<a href=\""+urlToFile+"\" download><img alt=\"download\" src=\""+downloadIcon+"\"></a>";
+	$(this).find( "span" ).first().append( downloadButtonHtml);
+
 });
 
 
